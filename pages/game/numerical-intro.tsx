ae5a049b7bddb0cbe1023faf1ec2b8bdbbdc77ac
Tommy Pranoto <tommyprn8@gmail.com>
2022-03-03 20:02:37 +0700

    fix halaman numerical intro mengikuti game terbaru, tanpa halaman latihan

--- pages/game/numerical-intro.tsx
+++ pages/game/numerical-intro.tsx
@@ -8,27 +8,36 @@ import SteerImage from '../../assets/Steering.png';
 
 import styles from './numerical-intro.module.scss';
 
-export interface NumericProps {
+interface NumericProps {
   nama: string;
 }
 
-export interface IntroProps {
+interface IntroProps {
   proctorImage: any;
   addCounter: () => void;
 }
 
-export interface TextProps {
+interface TextProps {
   text: string;
 }
 
-export interface explanationProps {
+interface ExplainationProps {
+  text: string;
+  position: string;
+}
+
+interface explanationProps {
   explanationText: string;
+}
+
+interface PanelProps {
+  border: string;
 }
 
 const Proctor: FC<IntroProps> = (props) => {
   const { proctorImage, addCounter } = props;
   return (
-    <div className={`${styles['proctorContainer']}`}>
+    <div className={styles['proctorContainer']}>
       <Image
         src={proctorImage}
         alt='proctor'
@@ -37,10 +46,10 @@ const Proctor: FC<IntroProps> = (props) => {
         layout='fixed'
       />
 
-      <p className={`${styles['proctorText']}`}>
+      <p className={styles['proctorText']}>
         Scan wajah untuk mengakses ruang kendali
       </p>
-      <button className={`${styles['submit']}`} onClick={() => addCounter()}>
+      <button className={styles['submit']} onClick={() => addCounter()}>
         Submit
       </button>
     </div>
@@ -50,53 +59,33 @@ const Proctor: FC<IntroProps> = (props) => {
 const Story: FC<TextProps> = (props) => {
   const { text } = props;
   return (
-    <div className={`${styles['storyContainer']}`}>
-      <p className={`${styles['storyText']}`}>{text}</p>
+    <div className={styles['storyContainer']}>
+      <p className={styles['storyText']}>{text}</p>
     </div>
   );
 };
 
 const Guide = () => {
+  return <div className={styles['guideContainer']} />;
+};
+
+const Panel: FC<PanelProps> = (props) => {
+  const { border } = props;
   return (
-    <div className={`${styles['guideContainer']}`}>
-      <div className={`${styles['guideScreen']}`} />
-      <div className={`${styles['guideScreen']}`} />
+    <div className={styles['panelContainer']}>
+      <div className={styles['ornament']} />
+      <div className={`${styles['panel']} ${styles[border]}`} />
+      <div className={styles['ornament']} />
     </div>
   );
 };
 
-const Steer = () => {
-  return (
-    <div className={`${styles['steerContainer']}`}>
-      <div className={`${styles['shapeWraper']}`}>
-        <div className={`${styles['oval']}`}></div>
-        <div className={`${styles['ovalShadow']}`}></div>
+const Explaination: FC<ExplainationProps> = (props) => {
+  const { position, text } = props;
 
-        <Image
-          src={SteerImage}
-          alt='Steering-wheel'
-          width={1000}
-          height={200}
-          layout='fixed'
-        />
-        <div className={`${styles['selectorWrapper']}`}>
-          <div className={`${styles['triangleLeft']}`}></div>
-          <div className={`${styles['triangleRight']}`}></div>
-        </div>
-        <div className={`${styles['shadowWrapper']}`}>
-          <div className={`${styles['triangleLeftShadow']}`}></div>
-          <div className={`${styles['triangleRightShadow']}`}></div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Explaination: FC<TextProps> = (props) => {
-  const { text } = props;
   return (
-    <div className={`${styles['explainContainer']}`}>
-      <p className={`${styles['explainText']}`}>{text}</p>
+    <div className={`${styles['explainContainer']} ${styles[position]}`}>
+      <p className={styles['explainText']}>{text}</p>
     </div>
   );
 };
@@ -105,53 +94,92 @@ const NumericalIntro: FC<NumericProps> = (props) => {
   const { nama } = props;
 
   const [renderBackground, setRenderBackground] = useState<string>('');
+  const [renderBorder, setRenderBorder] = useState<string>('');
+  const [renderPosition, setRenderPosition] = useState<string>('');
+
   const [visible, setVisible] = useState<boolean>(false);
-  const [counter, setCounter] = useState<number>(0);
+  const [counter, setCounter] = useState<number>(10);
 
   const [story] = useState([
     { id: 1, text: `hi ${nama}, selamat datang di ruang kendali!` },
-    { id: 2, text: `Kabar buruk!! Kita memasuki sabuk asteroid!` },
+    {
+      id: 2,
+      text: `Beberapa jam lalu terjadi gangguan pada pesawat yang 
+    belum diketahui penyebabnya. `,
+    },
     {
       id: 3,
-      text: `Fungsi otomatis untuk menghindari asteroid sedang tidak berfungsi!`,
+      text: `Hal ini menyebabkan beberapa sistem pada pesawat perlu
+      pengaturan ulang.`,
     },
     {
       id: 4,
-      text: `Kita harus mengemudikan pesawat secara manual, menggunakan sistem deteksi asteroid`,
+      text: `Ayo kita mulai dari Ruang Eksperimen Botani`,
     },
-    { id: 5, text: `Berdasarkan informasi dari sistem deteksi asteroid` },
+    {
+      id: 5,
+      text: `Ini adalah Ruang Eksperimen Botani untuk memantau berbagai
+    macam uji coba yang kami lakukan terhadap tanaman`,
+    },
     {
       id: 6,
-      text: `Kita harus menghindari titik dengan jumlah asteroid yang lebih banyak!`,
+      text: `Gangguan yang terjadi telah mengacaukan pengaturan
+      sehingga suhu dari tiap-tiap display perlu diatur ulang.`,
     },
-    { id: 7, text: `Terdapat 2 layar kiri dan kanan` },
+    {
+      id: 7,
+      text: `Apabila tidak dilakukan segera, akan sulit untuk menyelamatkan
+    tanaman - tanaman ini.`,
+    },
     {
       id: 8,
-      text: `Tekan tombol kanan apabila meteor di layar kanan lebih sedikit`,
+      text: `Pada kolom ini akan muncul temperatur 
+      yang diperlukan`,
     },
     {
       id: 9,
-      text: `Tekan tombol kiri apabila meteor di layar kiri lebih sedikit`,
+      text: `Pada kolom ini akan muncul beberapa pilihan. `,
     },
     {
       id: 10,
-      text: `Tentukan jumlah sesuai dengan operasi +, -, x, : yang ditunjukkan pada indikator ini!`,
+      text: `Pilih jawaban yang paling tepat untuk memperoleh
+      besar temperatur yang diperlukan`,
     },
     {
       id: 11,
-      text: `Mohon membaca instruksi secara teliti! anda tidak dapat kembali ke panel ini setelah menekan tombol mulai!`,
+      text: `Dan pada kolom ini akan muncul temperatur 
+      yang diperlukan`,
     },
   ]);
 
   useEffect(() => {
+    if (counter <= 0) {
+      setRenderBackground('enterance');
+    }
     if (counter >= 2) {
       setRenderBackground('control');
     }
-    if (counter > 3) {
+    if (counter > 6) {
+      setRenderBackground('hydroponics');
+    }
+    if (counter > 9) {
       setRenderBackground('mission');
     }
-    if (counter <= 0) {
-      setRenderBackground('enterance');
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter === 10) {
+      setRenderBorder('red');
+    } else {
+      setRenderBorder('');
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter > 10) {
+      setRenderPosition('top');
+    } else {
+      setRenderPosition('');
     }
   }, [counter]);
 
@@ -181,24 +209,28 @@ const NumericalIntro: FC<NumericProps> = (props) => {
       </Head>
 
       <main>
-        <div className={`${styles['missionContainer']}`}>
+        <div className={styles['missionContainer']}>
           {visible ? (
             <Proctor
               proctorImage={ProctorImage}
               addCounter={() => addCounter()}
             />
           ) : (
-            <div className={`${styles['buttonWraper']}`}>
+            <div className={styles['buttonWraper']}>
               <button
-                className={`${styles['button']}`}
+                className={styles['button']}
                 onClick={() => substractCounter()}
                 disabled={counter <= 0}
               >
                 {'<'}
               </button>
 
+              {counter >= 3 && counter <= 9 && (
+                <Story text={story[counter - 3].text} />
+              )}
+
               <button
-                className={`${styles['button']}`}
+                className={styles['button']}
                 onClick={() => addCounter()}
                 disabled={counter >= 13}
               >
@@ -207,38 +239,31 @@ const NumericalIntro: FC<NumericProps> = (props) => {
             </div>
           )}
 
-          {counter >= 3 && counter <= 9 && (
-            <Story text={story[counter - 3].text} />
-          )}
-
-          {counter >= 9 && (
+          {counter >= 10 && (
             <>
-              <Guide /> <Steer />
+              <Guide /> <Panel border={renderBorder} />{' '}
             </>
           )}
 
           {counter >= 10 && counter <= 13 && (
             <>
-              <Explaination text={story[counter - 3].text} />
+              <Explaination
+                text={story[counter - 3].text}
+                position={renderPosition}
+              />
               <div
                 className={
-                  counter === 10
-                    ? `${styles['lineHelperLeft']}`
-                    : counter === 11
-                    ? `${styles['lineHelperRight']}`
-                    : counter === 12
-                    ? `${styles['lineHelperMid']}`
-                    : `${styles['dummy']}`
+                  counter === 10 ? styles['lineHelper'] : styles['dummy']
                 }
               />
             </>
           )}
 
-          {counter === 13 && (
+          {/* {counter === 13 && (
             <button className={`${styles['startButton']}`}>
               <Link href='/game/numerical-test'>Mulai</Link>
             </button>
-          )}
+          )} */}
         </div>
       </main>
     </div>
